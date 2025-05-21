1469705c0074a5d432dd7a34cfbadac2c207835d
Kent Ng <kent.ng201@gmail.com>
2025-05-22 00:29:26 +0800

    fix jest conflict with tsconfig types, rename config to ts file

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,5 +1,18 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths || {},
+    { prefix: '<rootDir>/' }
+  ),
+  transform: {
+    "^.+\\.(ts|tsx)$": "./node_modules/ts-jest/preprocessor.js"
+  },
+  setupFiles: ['<rootDir>/src/utils/loadEnv.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
 };
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
